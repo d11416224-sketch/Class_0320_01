9c811128d154e24de8eb7419662615ae0030eacf
d11416224-sketch <d11416224@gs.takming.edu.tw>
2026-03-20 10:30:21 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Class_0320_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test joe = new Test();
+            Test mary = new Test();
+
+            Test tom;
+
+            //joe.Mid = 80;
+            //joe.Final = 76;
+            
+            joe.SetGrade(80, 76);
+
+            tom = joe;
+
+            //mary.Mid = 65;
+            //mary.Final = 80;
+
+            //Console.WriteLine("Joe" + joe.Mid);
+            //Console.WriteLine("Joe" + joe.Final);
+            Console.WriteLine("Joe" + joe.GetAvg().ToString());
+
+
+            //Console.WriteLine("Mary" + mary.Mid);
+            //Console.WriteLine("Mary" + mary.Final);
+
+
+            //joe.Mid = 59;
+            //joe.Final = 40;
+
+            //Console.WriteLine("Tom" + tom.Mid);
+            //Console.WriteLine("Tom" + tom.Final);
+
+
+        }
+    }
+}
